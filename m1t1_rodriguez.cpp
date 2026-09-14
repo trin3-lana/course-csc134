7136fb44f22b6c207381ab43057aa858733e722c
trin3-lana <lana13rodrigue@gmail.com>
2026-09-14 02:05:12 +0000

    Finished M1T1

---
+++ m1t1_rodriguez.cpp
@@ -0,0 +1,22 @@
+/*
+M1T1 - Hello World!
+Our first program exists to make sure we can talk to the user.
+We will use cout to print to the window.
+
+INPUT: None
+PROCESSING: None
+OUTPUT: The user's name as a greeting
+*/
+
+// magic words we will explain later
+#include <iostream>
+using namespace std;
+
+// Program returns as int (a number) - zero if everything worked.
+
+int main(){
+
+    cout << "Hello, world!";
+    cout << endl; // new line
+    return 0; // no errors
+}
